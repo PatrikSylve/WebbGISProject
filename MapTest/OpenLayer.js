abd8c570fe352769a30653f4b64a062d8c67bab9
Alfred Hildingson <hildingson@Alfreds-MacBook-Pro.local>
2018-12-18 13:35:12 +0100

    Made it work

--- MapTest/OpenLayer.js
+++ MapTest/OpenLayer.js
@@ -93,16 +93,88 @@ var public_buildings = new ol.source.ImageWMS({
       }),
     ];
 
-    var map;
-    require(["esri/map", "dojo/domReady!"], function(Map) {
-      map = new Map("map", {
-        basemap: "topo",
-        center: [-122.45, 37.75],
-        zoom: 13
+    var sourceBingMaps = new ol.source.BingMaps({
+        key: 'AqsqeZAt1OAOxK71ONck3RPobRs7_eZoxmgSyXfCL9ihKO7TjxAgGvzuFabQuYS1',
+        imagerySet: 'Road',
+        culture: 'fr-FR'
       });
-    });
+
+      var layers3 = [
+      bingMapsRoad = new ol.layer.Tile({
+        preload: Infinity,
+        source: sourceBingMaps
+      }),
+
+       bingMapsAerial = new ol.layer.Tile({
+        preload: Infinity,
+        source: new ol.source.BingMaps({
+          key: 'AqsqeZAt1OAOxK71ONck3RPobRs7_eZoxmgSyXfCL9ihKO7TjxAgGvzuFabQuYS1',
+          imagerySet: 'Aerial',
+        })
+      })];
+
+      t0 = new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+
+      t1 = new ol.layer.Image({
+        source: public_buildings
+      })
+
+      t2 = new ol.layer.Image({
+      source: roads_highway
+      })
+
+      t3 = new ol.layer.Image({
+      source: adresses
+      })
+
+      t4 = new ol.layer.Image({
+      source: roads_throug
+      })
+
+      t5 = new ol.layer.Image({
+      source: rural_buildings
+      })
+
+      t6 = new ol.layer.Image({
+      source: railroads
+      })
+
+      t7 = new ol.layer.Image({
+      source: roads_all
+      })
+
+      var layers = [t1,t2,t3,t4,t5];
+
+      var map = new ol.Map({
+        layers: [bingMapsAerial,t0],
+        target: 'map',
+        view: new ol.View({
+           center: ol.proj.fromLonLat([13.41, 55.55]),
+          zoom: 13
+        })
+      });
 
 
+      t0.on('precompose', function(event) {
+        var ctx = event.context;
+        var width = ctx.canvas.width * (swipe.value / 100);
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.rect(width, 0, ctx.canvas.width - width, ctx.canvas.height);
+        ctx.clip();
+      });
+
+      t0.on('postcompose', function(event) {
+        var ctx = event.context;
+        ctx.restore();
+      });
+
+      swipe.addEventListener('input', function() {
+        map.render();
+      }, false);
 
     // var map = new ol.Map({
     //   target: 'map',
@@ -112,13 +184,22 @@ var public_buildings = new ol.source.ImageWMS({
     //     zoom: 10
     //   })
     // });
+
+    // var map = new ol.Map({
+    //     layers: bingMapsRoad,
+    //     target: 'map',
+    //     view: new ol.View({
+    //       center: ol.proj.transform([6.562783, 46.517814], 'EPSG:4326', 'EPSG:3857'),
+    //       zoom: 13
+    //     })
+    //   });
     //
-    // function addLayer(box) {
-    //     var layerIndex = box.value;
-    //     if(box.checked){
-    //         map.addLayer(layers2[layerIndex]);
-    //     }
-    //     else {
-    //         map.removeLayer(layers2[layerIndex]);
-    //     }
-    //    }
+    function addLayer(box) {
+        var layerIndex = box.value;
+        if(box.checked){
+            map.addLayer(layers2[layerIndex]);
+        }
+        else {
+            map.removeLayer(layers2[layerIndex]);
+        }
+       }
