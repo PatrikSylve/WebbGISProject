18f81525998bf6c0d1a13797d358edec06211403
Alfred Hildingson <hildingson@Alfreds-MacBook-Pro.local>
2018-12-18 11:27:11 +0100

    added esri

--- MapTest/OpenLayer.js
+++ MapTest/OpenLayer.js
@@ -83,25 +83,32 @@ var public_buildings = new ol.source.ImageWMS({
       }),
     ];
 
-    
-
-
-
-    var map = new ol.Map({
-      target: 'map',
-      layers: layers2,
-      view: new ol.View({
-        center: ol.proj.fromLonLat([13.41, 55.55]),
-        zoom: 10
-      })
+    var map;
+    require(["esri/map", "dojo/domReady!"], function(Map) {
+      map = new Map("map", {
+        basemap: "topo",
+        center: [-122.45, 37.75],
+        zoom: 13
+      });
     });
 
-    function addLayer(box) {
-        var layerIndex = box.value;
-        if(box.checked){
-            map.addLayer(layers2[layerIndex]);
-        }
-        else {
-            map.removeLayer(layers2[layerIndex]);
-        }
-}
+
+
+    // var map = new ol.Map({
+    //   target: 'map',
+    //   layers: layers2,
+    //   view: new ol.View({
+    //     center: ol.proj.fromLonLat([13.41, 55.55]),
+    //     zoom: 10
+    //   })
+    // });
+    //
+    // function addLayer(box) {
+    //     var layerIndex = box.value;
+    //     if(box.checked){
+    //         map.addLayer(layers2[layerIndex]);
+    //     }
+    //     else {
+    //         map.removeLayer(layers2[layerIndex]);
+    //     }
+    //    }
